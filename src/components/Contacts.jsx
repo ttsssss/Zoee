8817044f91e620b544a0ae6ee4e36d070709c01b
ttsssss <sandovaltrent@gmail.com>
2022-07-14 20:57:11 -0600

    CSS positiong for new user button

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import '../App.css'
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import React from "react";
+import "../App.css";
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Contacts = () => {
-
-    return (
-        <div className="container">
-      <Button variant="primary">New User</Button>{' '}
-        </div>
-    );
-}
+  return (
+    <div className="container1">
+      <div className="container2">
+        <Button className="btn" variant="primary">
+          New User
+        </Button>{" "}
+      </div>
+    </div>
+  );
+};
 
 export default Contacts;
